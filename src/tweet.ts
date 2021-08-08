ecdce9bbf48aac5512f4863b84c34c74fab88425
Matt Bachmann <bachmann.matt@gmail.com>
2021-08-07 22:09:17 -0400

    specify node 16 for heroku

--- src/tweet.ts
+++ src/tweet.ts
@@ -74,7 +74,7 @@ function getDay(): number {
     const start = new Date(now.getFullYear(), 0, 0);
     const diff = now.valueOf() - start.valueOf() + (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
     const oneDay = 1000 * 60 * 60 * 24;
-    return Math.floor(diff / oneDay);
+    return Math.floor(diff / oneDay) + 1;
 }
 
 async function tweetHTMLConcept(): Promise<void> {
